5d308a02c7e8eb0e5ea655861a9e33401d653683
OlmerOnline <OlmerOnline@mail.ru>
2017-08-17 15:13:03 +0300

    create project

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Гостевая книга',
+	'description' => 'Гостевая книга',
+	'keywords' => 'Гостевая книга',
+	'robots' => 'index, follow'
+);
+?>
